354e73ec0f3467bd4b0e4851a2b88d0a5ff76b03
keipala <keipalahniuk@gmail.com>
2022-10-18 20:59:47 +0200

    Updated index page to new user model.

--- SunsetsIO/Pages/Rate/Index.cshtml.cs
+++ SunsetsIO/Pages/Rate/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -12,11 +13,13 @@ namespace SunsetsIO.Pages.Rate
 {
     public class IndexModel : PageModel
     {
-        private readonly SunsetsIO.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
 
-        public IndexModel(SunsetsIO.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Rating> Rating { get;set; } = default!;
@@ -26,7 +29,9 @@ namespace SunsetsIO.Pages.Rate
             if (_context.Rating != null)
             {
                 Rating = await _context.Rating
-                .Include(r => r.User).ToListAsync();
+                .Include(r => r.User)
+                .Where(a => a.User.UserName == User.Identity.Name)
+                .ToListAsync();
             }
         }
     }
